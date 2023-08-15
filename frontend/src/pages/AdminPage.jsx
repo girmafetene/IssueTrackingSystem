96b260f1b4005aad11e0391116391b07d65b034d
Meron-kedir <88824826+Meron-kedir@users.noreply.github.com>
2023-08-15 19:37:41 +0300

    my files

---
+++ frontend/src/pages/AdminPage.jsx
@@ -0,0 +1,10 @@
+function Admin(){
+    return (
+        <>
+        <div className="h-screen w-full bg-cyan-950" >
+          <div className="text-white font-extrabold text-3xl"> Welcome Admin</div>
+        </div>
+        </>
+    )
+}
+export default Admin; 
