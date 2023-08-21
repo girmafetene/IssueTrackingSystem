6971c9b05c5cc08bb04842fc6ef8cce9e8d07a8a
yonas sisay <yonimelkamu357@gmail.com>
2023-08-21 20:30:44 +0300

    create table

---
+++ admin/src/pages/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideBar from '../components/SideBar'
+import Table from '../components/Table'
+import DropDown from '../components/DropDown'
+function homepage() {
+  return (
+    <div>
+        <SideBar/>
+        <Table/>
+        <DropDown/>
+    </div>
+  )
+}
+
+export default homepage
